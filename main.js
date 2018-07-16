44aecce721b6850a746a0a9b2066a615f5a34ccb
Furkan Arabacı <illegaldisease@gmail.com>
2018-07-16 14:17:59 +0300

    outline added

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let http = require('http');
-
+//Node server is just there for future player vs player feature. Not needed for now.
 http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Hello World!');
